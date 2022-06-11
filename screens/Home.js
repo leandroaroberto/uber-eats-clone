8195ae3e613b28325cdf8294af413cbc50afb5ad
Leandro Roberto <leandro.roberto@dextra-sw.com>
2022-06-11 00:00:59 -0300

    HeaderTabs component

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,12 @@
 import { View, Text, SafeAreaView, Platform, StatusBar } from 'react-native'
 import React from 'react'
+import HeaderTabs from '../components/HeaderTabs'
 import { SaferAreaView } from '../components/SaferAreaView'
 
 export default function Home() {
   return (
     <SaferAreaView>
-        <Text>Tabs</Text>
+      <HeaderTabs/>
     </SaferAreaView>
   )
 }
