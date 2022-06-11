7ee9736be50e13d2a839e030e30d97a01e7e1f58
Leandro Roberto <leandro.roberto@dextra-sw.com>
2022-06-10 23:56:48 -0300

    Home page and SaferAreaView component

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView, Platform, StatusBar } from 'react-native'
+import React from 'react'
+import { SaferAreaView } from '../components/SaferAreaView'
+
+export default function Home() {
+  return (
+    <SaferAreaView>
+        <Text>Tabs</Text>
+    </SaferAreaView>
+  )
+}
