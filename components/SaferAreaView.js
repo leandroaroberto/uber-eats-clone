9acf84f49d677868cc5ffac64cc53490b90a01f3
Leandro Roberto <leandro.roberto@dextra-sw.com>
2022-06-14 07:22:40 -0300

    Search component

--- components/SaferAreaView.js
+++ components/SaferAreaView.js
@@ -3,9 +3,9 @@ import { Platform, View, StatusBar, SafeAreaView } from "react-native";
 
 export function SaferAreaView({ children }) {
   if (Platform.OS == "ios") {
-    return <SafeAreaView style={{ flex: 1 }}><StatusBar style="auto"/>{children}</SafeAreaView>;
+    return <SafeAreaView style={{ flex: 1 , backgroundColor: '#eee'}}>{children}</SafeAreaView>;
   }
   if (Platform.OS == "android") {
-    return <View style={{flex: 1, paddingTop: StatusBar.currentHeight}}>{children}</View>;
+    return <View style={{flex: 1, backgroundColor: '#eee', paddingTop: StatusBar.currentHeight }}>{children}</View>;
   }
 }
--- screens/Home.js
+++ screens/Home.js
@@ -2,11 +2,15 @@ import { View, Text, SafeAreaView, Platform, StatusBar } from 'react-native'
 import React from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import { SaferAreaView } from '../components/SaferAreaView'
+import SearchBar from '../components/SearchBar'
 
 export default function Home() {
   return (
     <SaferAreaView>
-      <HeaderTabs/>
+      <View style={{backgroundColor: 'white', padding: 15}}>
+        <HeaderTabs/>
+        <SearchBar/>
+      </View>
     </SaferAreaView>
   )
 }
