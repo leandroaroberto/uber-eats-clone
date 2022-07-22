47f2b929881357e829dccfbe6b25f5e9579febf8
Leandro Roberto <leandro.roberto@dextra-sw.com>
2022-07-22 15:21:53 -0300

    fix StatusBar for IOS

--- components/SaferAreaView.js
+++ components/SaferAreaView.js
@@ -3,9 +3,14 @@ import { Platform, View, StatusBar, SafeAreaView } from "react-native";
 
 export function SaferAreaView({ children }) {
   if (Platform.OS == "ios") {
-    return <SafeAreaView style={{ flex: 1 , backgroundColor: '#eee'}}>{children}</SafeAreaView>;
+    StatusBar.setBarStyle('dark-content');
+    return <SafeAreaView style={{ flex: 1 , backgroundColor: '#eee'}}>
+        {children}
+      </SafeAreaView>;
   }
   if (Platform.OS == "android") {
+    StatusBar.setTranslucent(true);
+    StatusBar.setBackgroundColor('transparent');
     return <View style={{flex: 1, backgroundColor: '#eee', paddingTop: StatusBar.currentHeight }}>{children}</View>;
   }
 }
